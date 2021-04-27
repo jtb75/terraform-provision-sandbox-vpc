25ea88c6b801fa0d5efb5509c936f6a9cdd3529e
Joe Buhr <jtb75@users.noreply.github.com>
2021-04-27 14:33:12 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,93 @@
+# declare a VPC
+resource "aws_vpc" "sandbox" {
+  cidr_block       = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Sandbox VPC"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.sandbox.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "sandbox_igw" {
+  vpc_id = aws_vpc.sandbox.id
+
+  tags = {
+    Name = "Sandbox VPC - Internet Gateway"
+  }
+}
+
+resource "aws_route_table" "sandbox_us_east_1a_public" {
+    vpc_id = aws_vpc.sandbox.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.sandbox_igw.id
+    }
+
+    tags = {
+        Name = "Public Subnet Route Table"
+    }
+}
+
+resource "aws_route_table_association" "sandbox_us_east_1a_public" {
+    subnet_id = aws_subnet.public.id
+    route_table_id = aws_route_table.sandbox_us_east_1a_public.id
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh_sg"
+  description = "Allow SSH inbound connections"
+  vpc_id = aws_vpc.sandbox.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_sg"
+  }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http_sg"
+  description = "Allow HTTP inbound connections"
+  vpc_id = aws_vpc.sandbox.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_http_sg"
+  }
+}
