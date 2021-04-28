b3d05e7d28182a2355567f28cd92e4ba16a89e65
Joe Buhr <jtb75@users.noreply.github.com>
2021-04-28 12:49:06 -0500

    added flowlogs

--- flow_logs.tf
+++ flow_logs.tf
@@ -1,1 +1,54 @@
+resource "aws_flow_log" "pc_flow" {
+  iam_role_arn    = aws_iam_role.pc_flow_role.arn
+  log_destination = aws_cloudwatch_log_group.pc_flow_log.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.sandbox.id
+}
 
+resource "aws_cloudwatch_log_group" "pc_flow_log" {
+  name = "pc_flow_log"
+}
+
+resource "aws_iam_role" "pc_flow_role" {
+  name = "pc_flow_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "vpc-flow-logs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "pc_flow_policy" {
+  name = "pc_flow_policy"
+  role = aws_iam_role.pc_flow_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
