63d68f7a8d243c5f22c5ab87310c96fc7db6d0b7
Joe Buhr <jtb75@users.noreply.github.com>
2021-05-03 07:28:15 -0500

    updated utility role

--- ec2-deployments.tf
+++ ec2-deployments.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "utility_instance" {
   vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
   subnet_id = aws_subnet.public.id
   associate_public_ip_address = true
+  iam_instance_profile = "ssm-mgr-role"
 
   tags = {
     Name = "utility-instance"
