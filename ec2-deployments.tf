375a1aeff0a23798706544e9a0c14c73ff001b8f
Joe Buhr <jtb75@users.noreply.github.com>
2021-05-05 07:37:00 -0500

    removed utility ec2

--- ec2-deployments.tf
+++ ec2-deployments.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_instance" "utility_instance" {
   ami           = "ami-042e8287309f5df03"
   instance_type = "t2.micro"
@@ -15,6 +16,7 @@ resource "aws_instance" "utility_instance" {
 output "utility_instance_public_ip" {
   value = "aws_instance.utility_instance.public_ip"
 }
+*/
 
 /*
 resource "aws_instance" "docker_instance" {
