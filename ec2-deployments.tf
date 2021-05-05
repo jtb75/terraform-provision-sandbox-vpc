e0723f24d976a2f4ab712b5f5e8609a551d0fec9
Joe Buhr <jtb75@users.noreply.github.com>
2021-05-05 08:03:37 -0500

    added Defender tag

--- ec2-deployments.tf
+++ ec2-deployments.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "utility_instance" {
 }
 
 output "utility_instance_public_ip" {
-  value = "aws_instance.utility_instance.public_ip"
+  value = "${aws_instance.utility_instance.public_ip}"
 }
 
 /*
@@ -43,10 +43,11 @@ resource "aws_instance" "web_instance" {
   vpc_security_group_ids = [ aws_security_group.allow_ssh.id, aws_security_group.allow_http.id ]
   subnet_id = aws_subnet.public.id
   associate_public_ip_address = true
+  iam_instance_profile = "ssm-mgr-role"
 
   tags = {
     Name = "web-instance"
-  //  Defender = "true"
+    Defender = "true"
   }
 }
 
