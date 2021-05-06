34a68e0490c11642480aa9b3f6786aaacd1bb91e
Joe Buhr <jtb75@users.noreply.github.com>
2021-05-06 07:37:42 -0500

    docker ec2 instance

--- ec2-deployments.tf
+++ ec2-deployments.tf
@@ -17,7 +17,6 @@ output "utility_instance_public_ip" {
   value = "${aws_instance.utility_instance.public_ip}"
 }
 
-/*
 resource "aws_instance" "docker_instance" {
   ami           = "ami-042e8287309f5df03"
   instance_type = "t2.micro"
@@ -25,6 +24,7 @@ resource "aws_instance" "docker_instance" {
   vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
   subnet_id = aws_subnet.public.id
   associate_public_ip_address = true
+  iam_instance_profile = "ssm-mgr-role"
 
   tags = {
     Name = "docker-instance"
@@ -34,7 +34,6 @@ resource "aws_instance" "docker_instance" {
 output "docker_instance_public_ip" {
   value = "${aws_instance.docker_instance.public_ip}"
 }
-*/
 
 resource "aws_instance" "web_instance" {
   ami           = "ami-042e8287309f5df03"
