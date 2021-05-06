a797699987542a2a6ab302b1fcf2a6de01947148
Joe Buhr <jtb75@users.noreply.github.com>
2021-05-06 13:23:06 -0500

    .

--- ec2-deployments.tf
+++ ec2-deployments.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "utility_instance" {
 output "utility_instance_public_ip" {
   value = "${aws_instance.utility_instance.public_ip}"
 }
-
+/*
 resource "aws_instance" "docker_instance" {
   ami           = "ami-042e8287309f5df03"
   instance_type = "t2.micro"
@@ -35,7 +35,7 @@ resource "aws_instance" "docker_instance" {
 output "docker_instance_public_ip" {
   value = "${aws_instance.docker_instance.public_ip}"
 }
-
+*/
 resource "aws_instance" "web_instance" {
   ami           = "ami-042e8287309f5df03"
   instance_type = "t2.micro"
