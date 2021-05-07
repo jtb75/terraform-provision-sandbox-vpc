5b94e76ee8c57b7ac72775032e5256c2d6dc7761
Joe Buhr <jtb75@users.noreply.github.com>
2021-05-07 10:02:12 -0500

    added test instance

--- ec2-deployments.tf
+++ ec2-deployments.tf
@@ -54,3 +54,22 @@ resource "aws_instance" "web_instance" {
 output "web_instance_public_ip" {
   value = "${aws_instance.web_instance.public_ip}"
 }
+
+resource "aws_instance" "test_instance" {
+  ami           = "ami-042e8287309f5df03"
+  instance_type = "t2.micro"
+  key_name = "nv-pan"
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id, aws_security_group.allow_http.id ]
+  subnet_id = aws_subnet.public.id
+  associate_public_ip_address = true
+  iam_instance_profile = "ssm-mgr-role"
+
+  tags = {
+    Name = "test-instance"
+    Defender = "true"
+  }
+}
+
+output "test_instance_public_ip" {
+  value = "${aws_instance.test_instance.public_ip}"
+}
